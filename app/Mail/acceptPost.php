6018c8e06442d339db27916d4aef55af30752b4b
poni <poni@habitattt.it>
2023-07-09 14:12:12 +0200

    mail

---
+++ app/Mail/acceptPost.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class acceptPost extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public $id;
+    public $name;
+    public $post;
+    public $img;
+   public function __construct($id, $name, $post, $img)
+   {
+       $this->id = env('APP_URL') . "crescione/" . $id;
+       $this->name = $name;
+       $this->post = $post;
+       $this->img = env('APP_URL') . $img;
+   }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Nuovo Post!',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'accept.post',
+            with: [
+                'name' => $this->name,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
